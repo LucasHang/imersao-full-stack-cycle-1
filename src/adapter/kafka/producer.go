e810232d11e4e94a7e54203fe9b3b0af3133e364
Lucas Hang <lucas.hhang@gmail.com>
2021-12-09 07:26:42 -0300

    feat: kafka producer

---
+++ src/adapter/kafka/producer.go
@@ -0,0 +1,45 @@
+package kafka
+
+import (
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/lucashang/imersao5-gateway/src/adapter/presenter"
+)
+
+type Producer struct {
+	ConfigMap *ckafka.ConfigMap
+	Presenter presenter.Presenter
+}
+
+func NewKafkaProducer(configMap *ckafka.ConfigMap, presenter presenter.Presenter) *Producer {
+	return &Producer{ConfigMap: configMap, Presenter: presenter}
+}
+
+func (p *Producer) Publish(msg interface{}, key []byte, topic string) error {
+	producer, err := ckafka.NewProducer(p.ConfigMap)
+	if err != nil {
+		return err
+	}
+
+	err = p.Presenter.Bind(msg)
+	if err != nil {
+		return err
+	}
+
+	presenterMsg, err := p.Presenter.Show()
+	if err != nil {
+		return err
+	}
+
+	message := &ckafka.Message{
+		TopicPartition: ckafka.TopicPartition{Topic: &topic, Partition: int32(ckafka.PartitionAny)},
+		Value:          presenterMsg,
+		Key:            key,
+	}
+
+	err = producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
